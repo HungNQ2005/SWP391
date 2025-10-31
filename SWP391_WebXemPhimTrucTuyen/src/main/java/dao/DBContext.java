796ec68cc91083f30bbb85f8a13b76d08d97d61c
nicky949 <cuongct.ce190026@gmail.com>
2025-11-01 00:11:53 +0700

    merge xong branch Phi Yen

--- SWP391_WebXemPhimTrucTuyen/src/main/java/dao/DBContext.java
+++ SWP391_WebXemPhimTrucTuyen/src/main/java/dao/DBContext.java
@@ -20,7 +20,7 @@ import java.util.logging.Logger;
 public class DBContext {
 
     private Connection conn;
-    private final String DB_URL = "jdbc:sqlserver://127.0.0.1:1433;databaseName=webFilm;encrypt=false";
+    private final String DB_URL = "jdbc:sqlserver://127.0.0.1:1433;databaseName=webFilm_Final;encrypt=false";
     private final String DB_USER = "sa";
     private final String DB_PWD = "123456";
 
