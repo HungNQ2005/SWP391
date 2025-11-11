f5e068c532f25e0bd11ebe1b78d313265a438b13
nicky949 <cuongct.ce190026@gmail.com>
2025-11-11 11:56:16 +0700

    Ignore target folder

--- SWP391_WebXemPhimTrucTuyen/target/ROOT/js/ViewMovieBanner.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".film-link").forEach((link) => {
-    link.addEventListener("click", (e) => {
-      e.preventDefault();
-      const movieId = link.dataset.id;
-      window.location.href = `/xem-phim/?id=${movieId}`; // chen id theo model
-    });
-  });
-});
--- SWP391_WebXemPhimTrucTuyen/target/ROOT/profile.jsp
+++
@@ -1,9 +0,0 @@
-<%@ page import="entity.User" %>
-<%
-    User user = (User) session.getAttribute("guest");
-%>
-<h2>Xin chào, <%= user.getUsername() %></h2>
-
-<img src="<%= user.getAvatar_url() %>" width="150" height="150" alt="Avatar">
-
-<br><a href="updateAvatar.jsp">C?p nh?t ?nh ??i di?n</a>
